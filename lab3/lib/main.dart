ec550997e4e8758d8617f552ef3ae34e7efb53f7
VLLindqvist <victor.lindquist@outlook.com>
2020-12-13 11:01:57 +0100

    Lab 3 done

--- lab3/lib/main.dart
+++ lab3/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lab3/routes/githubRepoPage.dart';
-import 'package:lab3/githubRepoListItem.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:lab3/routes/githubRepoPage.dart';
+import 'package:lab3/githubRepoListItem.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 Future main() async {
